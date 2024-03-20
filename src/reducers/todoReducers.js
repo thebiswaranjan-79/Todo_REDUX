6e2fda6c3d2bb3553dbd060508f066dd0ff90f48
Biswaranjan <mbiswaranjan797@gmail.com>
2024-03-21 00:18:27 +0530

    Adding Reducers

---
+++ src/reducers/todoReducers.js
@@ -0,0 +1,22 @@
+import  {ADD_TODO,REMOVE_TODO, EDIT_TODO} from '../constants/index';
+
+function todoReducer(todos =[], action){
+    if(action.type == ADD_TODO){
+        return [...todos, {id: action.payload.id, title:action.payload.title}]
+    }
+    if(actions.type == EDIT_TODO){
+        return todos.filter(todo => todo.id != action.payload);
+    }
+    if(actions.type == REMOVE_TODO){
+        return todos.map(todo => {
+            if(todo.id == action.payload.id){
+                todo.title = action.payload.title;
+            }
+            return todo;
+        })
+    }
+
+    return todos;
+}
+
+export default todoReducer;
